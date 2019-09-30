6abc857248e4227556f2b5f38a0953f1c464c4ac
Plugway <48772861+Plugway@users.noreply.github.com>
2019-09-30 15:01:23 +0500

    InterruptedException deleted

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
-import java.util.Random;
-import java.util.Scanner;
 
 
 public class Main
 {
     public static String QuestPath = "src/q&a.txt";     //should work in both Windows and Linux
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         Logic.start();
     }
 }
