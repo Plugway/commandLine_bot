8c3db80a8291b249bfce505ff55b3a6c0021eb1d
ReptileSerpent <55386920+ReptileSerpent@users.noreply.github.com>
2019-10-07 14:09:32 +0500

    Changes in Logic.java
    Replaced a few console calls that accidently missed before in Logic.java.
    Renamed runProgram() to runQuiz().
    Renamed getQuestionNumber() to getTotalQuestionsToAsk().
    Renamed maxQuestNum to totalQuestionsToAsk.
    Renamed givenQuestNum to questionsAskedQuantity.
    Wrote handleUserQuizInput(), including the try catch for answers.

--- src/Logic.java
+++ src/Logic.java
@@ -4,8 +4,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Logic {
-    private static Random rnd = new Random(System.currentTimeMillis());
-    private static Scanner in = new Scanner(System.in); //убрать
     private static final String styleDelimeter = ")";
     private static String helpText = "Привет, я бот, вот что я умею:\n" +
             "1. Задавать вопросики, на которые тебе нужно ответить. В вопросе может быть несколько верных отетов, которые пишутся через запятую, например, \"2,3\"\n" +
@@ -17,6 +15,8 @@ public class Logic {
     private static IO botIO;
 
     private static boolean quitQuestFlag = false;
+
+    private static Random rnd = new Random(System.currentTimeMillis());
     private static int getRandom(int min, int max)
     {
         return min + rnd.nextInt(max - min + 1);
@@ -26,7 +26,7 @@ public class Logic {
         selectIOClass();    //выбирается в зависимости от botMode
 
         botIO.println(helpText);
-        resolveCommand(in.next().substring(1));
+        resolveCommand(botIO.readUserQuery().substring(1));
     }
 
     private static void selectIOClass()
@@ -45,63 +45,78 @@ public class Logic {
         }
     }
 
-    private static void runProgram() throws IOException {
+    private static void runQuiz() throws IOException {
         var questions = Question.parseQuestions(Main.QuestPath);
         var totalQuestionsAvailable = questions.size();
-        var givenQuestNum = 0;
-        var maxQuestNum = getQuestionNumber(totalQuestionsAvailable);
+        var questionsAskedQuantity = 0;
         var score = 0;
+
+        var totalQuestionsToAsk = getTotalQuestionsToAsk(totalQuestionsAvailable);
 
         while (!quitQuestFlag)
         {
-            if (givenQuestNum == totalQuestionsAvailable || givenQuestNum == maxQuestNum)   //если все вопросы были то конец
-                break;
+            if (questionsAskedQuantity == totalQuestionsAvailable || questionsAskedQuantity == totalQuestionsToAsk)
+                break;                                              //если все вопросы были то конец
 
             var currentQuestionNum = getNextQuestionNum(questions, totalQuestionsAvailable);
             var currentQuestion = questions.get(currentQuestionNum);
 
-            botIO.println((givenQuestNum+1) + styleDelimeter + currentQuestion.getQuestion()); //печатаем вопрос
-            for (var i = 0; i < currentQuestion.getAnswers().size(); i++)           //печатаем ответы
+            botIO.println((questionsAskedQuantity+1) + styleDelimeter + currentQuestion.getQuestion());         //печатаем вопрос
+            for (var i = 0; i < currentQuestion.getAnswers().size(); i++)                                       //печатаем ответы
                 botIO.println(currentQuestion.getAnswers().get(i));
 
-            var input = botIO.readUserQuery();
-            if (input.substring(0, 1).equals("/"))
-            {
-                resolveCommand(input.substring(1));
-                continue;
-            }
-            //try
-            var intInput = getIntInputArray(input);
-            //catch неправильный ввод
+            var intInput = handleUserQuizInput();
 
             if (isAnswersRight(intInput, currentQuestion.getRightAnswers()))
                 score++;
             currentQuestion.setHasBeen();
             questions.set(currentQuestionNum, currentQuestion);
-            givenQuestNum++;
+            questionsAskedQuantity++;
         }
-        botIO.println("Твой счет: " + score + " из " + givenQuestNum);
+        botIO.println("Твой счет: " + score + " из " + questionsAskedQuantity);
+    }
+
+    private static int[] handleUserQuizInput() throws IOException {
+        int[] intInput = new int[0];
+
+        var answerIsGiven = false;
+        while (!answerIsGiven)
+        {
+            var input = botIO.readUserQuery();
+            if (input.substring(0, 1).equals("/")) {
+                resolveCommand(input.substring(1));
+                continue;
+            }
+            try {
+                intInput = getIntInputArray(input);
+                answerIsGiven = true;
+            } catch (Exception e) {
+                botIO.println("Введите числа, соответствующие ответам, через запятую");
+            }
+        }
+        return intInput;
     }
 
     private static int getNextQuestionNum(List<Question> questions, int totalQuestionsAvailable)
     {
         var nextQuestionNum = getRandom(1, totalQuestionsAvailable);
-        while (questions.get(nextQuestionNum-1).getHasBeen())            //если такой вопрос был
+        while (questions.get(nextQuestionNum-1).getHasBeen())                       //если такой вопрос был
         {
-            nextQuestionNum = ((nextQuestionNum)%totalQuestionsAvailable)+1;         //двигаемся вперед пока не найдем
+            nextQuestionNum = ((nextQuestionNum)%totalQuestionsAvailable)+1;        //двигаемся вперед пока не найдем
         }
         return nextQuestionNum-1;
     }
-    private static int getQuestionNumber(int totalQuestionsAvailable)
+
+    private static int getTotalQuestionsToAsk(int totalQuestionsAvailable)
     {
         botIO.println("Сколько вопросов? Общее число вопросов:" + totalQuestionsAvailable);
-        int maxQuestNum=0;
+        int totalQuestionsToAsk=0;
         var inputIsNumber = false;
         var inputIsGood = false;
         while (!inputIsNumber & !inputIsGood) {
             try {
-                maxQuestNum = Integer.parseInt(in.next());
-                if (maxQuestNum <= totalQuestionsAvailable)
+                totalQuestionsToAsk = Integer.parseInt(botIO.readUserQuery());
+                if (totalQuestionsToAsk <= totalQuestionsAvailable)
                 {
                     inputIsGood = true;
                     inputIsNumber = true;
@@ -112,8 +127,9 @@ public class Logic {
                 botIO.println("Введите число");
             }
         }
-        return maxQuestNum;
+        return totalQuestionsToAsk;
     }
+
     private static boolean isAnswersRight(int[] userInput, List<Integer> rightInput)
     {
         var rightCounter = 0;
@@ -126,6 +142,7 @@ public class Logic {
         }
         return rightCounter == userInput.length;
     }
+
     private static int[] getIntInputArray(String input)
     {
         var numinput = input.replaceAll(" ", "").split(",");
@@ -134,11 +151,12 @@ public class Logic {
             intInput[j] = Integer.parseInt(numinput[j]);
         return intInput;
     }
+
     private static void resolveCommand(String command) throws IOException {
         switch (command)
         {
             case "start":
-                runProgram();
+                runQuiz();
                 break;
             case "help":
                 System.out.print(helpText);
